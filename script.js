276e9b3ca6c14be963e2c5c864e98fd5dc1432bf
Ndekestro <ndekezibenjamin2@gmail.com>
2024-06-06 21:54:22 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.gallery-item').forEach(item => {
+    item.addEventListener('mouseover', () => {
+        item.classList.add('hover');
+    });
+    item.addEventListener('mouseout', () => {
+        item.classList.remove('hover');
+    });
+});
